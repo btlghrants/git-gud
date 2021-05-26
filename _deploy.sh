be7830111910f9ae176211e8634ac0d9750f9567
Barrett LaFrance <barrett@leapfrog.ai>
2021-05-26 17:11:57 -0400

    add index.html-->404.html hack to allow for HTML5 History mode in app

--- _deploy.sh
+++ _deploy.sh
@@ -14,13 +14,14 @@ remote=$(git remote -v | grep '(push)' | awk '{print $2}')
 # navigate into the build output directory
 cd dist
 
+# enable HTML5 history mode in vue-router via "404.html hack"
+# https://stackoverflow.com/questions/47677220/vuejs-history-mode-with-github-gitlab-pages
+mv index.html 404.html
+
 # get output dir ready to push via git
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 # local branch is master or main?
 branch=$(git branch --show-current)
