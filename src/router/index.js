d7ce046b5f1722af89094dcff02a116984da3f5a
Barrett LaFrance <barrett@leapfrog.ai>
2021-06-11 14:55:07 -0400

    wip: init /resources as default route

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '@/views/Home.vue'
-import FourOhFour from '@/views/404.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Resources from '@/views/Resources.vue';
+import FourOhFour from '@/views/404.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: { name: 'Resources' }
+  },
+  {
+    path: '/resources',
+    name: 'Resources',
+    component: Resources
   },
   {
     path: '*',
     name: '404',
     component: FourOhFour
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
